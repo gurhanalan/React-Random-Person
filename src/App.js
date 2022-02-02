1bb2fbf88a4b69a4490350891631e81615566f75
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-02-02 21:58:53 +0300

    add states and img

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import {
-  FaEnvelopeOpen,
-  FaUser,
-  FaCalendarTimes,
-  FaMap,
-  FaPhone,
-  FaLock,
-} from 'react-icons/fa'
-const url = 'https://randomuser.me/api/'
-const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
+    FaEnvelopeOpen,
+    FaUser,
+    FaCalendarTimes,
+    FaMap,
+    FaPhone,
+    FaLock,
+} from "react-icons/fa";
+const url = "https://randomuser.me/api/";
+const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
 function App() {
-  return <h2>random user starter</h2>
+    const [loading, setLoading] = useState(true);
+    const [person, setPerson] = useState(null);
+    const [title, setTitle] = useState("name");
+    const [value, setValue] = useState("random person");
+
+    const handleValue = (e) => {
+        console.log(e.target);
+    };
+    return (
+        <main>
+            <div className="block bcg-black"></div>
+            <div className="block">
+                <div className="container">
+                    <img
+                        src={defaultImage}
+                        alt="random user"
+                        className="user-img"
+                    />
+                </div>
+            </div>
+        </main>
+    );
 }
 
-export default App
+export default App;
