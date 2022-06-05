f415723b66448c3abe036c9239ff3d31900f30fe
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2022-06-06 00:18:07 +0300

    Updates

---
+++ precache-manifest.aebd3432ced34cd6e250be97620fddef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f7bbd99b83c5f03768592427e5a11b8",
+    "url": "/React-Random-Person/index.html"
+  },
+  {
+    "revision": "39be7059c2146ed78400",
+    "url": "/React-Random-Person/static/css/main.5921d42c.chunk.css"
+  },
+  {
+    "revision": "5bf2c449ea131aa1d216",
+    "url": "/React-Random-Person/static/js/2.a987e3f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Random-Person/static/js/2.a987e3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39be7059c2146ed78400",
+    "url": "/React-Random-Person/static/js/main.b0085aef.chunk.js"
+  },
+  {
+    "revision": "b836db356a888132c131",
+    "url": "/React-Random-Person/static/js/runtime-main.cf6d9ecc.js"
+  }
+]);
